9bf112512edd867803201a2f225796f6d659a892
Mauri <msosa1011@gmail.com>
2017-02-06 20:30:14 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Despliegue de Aplicaciones LAMP</title>
+</head>
+<body>
+	<h1>Despliegue de aplicaciones LAMP</h1>
+<?php
+	phpinfo();
+?>
+</body>
+</html>
